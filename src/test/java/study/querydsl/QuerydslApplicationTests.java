3c93d3925edd351fa739a570b28aeb09056784d5
taehyun9244 <85472257+taehyun9244@users.noreply.github.com>
2022-05-24 18:53:28 +0900

    Initial commit

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,15 @@
+package study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
